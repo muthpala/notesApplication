a0079923ad19fdd55883b028fd1693d7df035618
Muthukumar <muthu101994@gmail.com>
2019-04-04 08:32:32 +0530

    refs #notesApplication changes for war file deploy commit

--- notes-application/src/main/java/com/wander/notesapp/notesapplication/NotesApplication.java
+++ notes-application/src/main/java/com/wander/notesapp/notesapplication/NotesApplication.java
@@ -4,15 +4,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 
 @SpringBootApplication
-public class NotesApplication implements WebMvcConfigurer{
+public class NotesApplication extends SpringBootServletInitializer implements WebMvcConfigurer{
 
 	private static final Logger logger = LoggerFactory.getLogger(NotesApplication.class);
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(NotesApplication .class);
+    }
 	
 	public static void main(String[] args) {
 		SpringApplication.run(NotesApplication.class, args);
@@ -24,11 +29,6 @@ public class NotesApplication implements WebMvcConfigurer{
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/");
 
-    }
-
-    @Bean
-    public Java8TimeDialect java8TimeDialect() {
-        return new Java8TimeDialect();
     }
 
 }
